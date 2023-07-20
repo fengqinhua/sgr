472489fbaa1052edee4cfc73039149f0cead4fc8
fengqinhua2016@163.com <fengqinhua2016@1653.com>
2023-07-20 17:22:09 +0800

    添加第一个项目

--- docs/work_log.md
+++ docs/work_log.md
@@ -1,8 +1,8 @@
+# 开发日志
 
+## 2023-07 
 
-**说明**
-
-# 一、git 子模块
+### 01. git 子模块
 
 如果某个工作中的项目需要包含并使用另一个项目。 也许是第三方库，或者你独立开发的，用于多个父项目的库，此时可使用git子模块。
 
@@ -12,4 +12,50 @@
 2. 克隆含有子模块的项目 ： 当使用git clone 克隆一个含有子模块的项目时，默认会包含该子模块目录，但其中还没有任何文件。你必须运行两个命令：git submodule init 用来初始化本地配置文件，而 git submodule update 则从该项目中抓取所有数据并检出父项目中列出的合适的提交。
 另外，还有一种更简单的方式， 如果给 git clone 命令传递 --recurse-submodules 选项，它就会自动初始化并更新仓库中的每一个子模块， 包括可能存在的嵌套子模块。
  
- 参考： https://git-scm.com/book/zh/v2/Git-%E5%B7%A5%E5%85%B7-%E5%AD%90%E6%A8%A1%E5%9D%97
+ 参考： 
+
+ > [Git 工具 - 子模块]( https://git-scm.com/book/zh/v2/Git-%E5%B7%A5%E5%85%B7-%E5%AD%90%E6%A8%A1%E5%9D%97)
+
+### 02. VS类增加默认的注释信息
+
+方法：
+
+1. 寻找VS中配置类模板的目录，例如我的就在：C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\ItemTemplates\CSharp\Code\2052\Class
+2. 打开目录中的Class.cs文件进行修改，增加注释信息，如下：
+~~~code
+/**************************************************************
+ * 
+ * 唯一标识：$guid10$
+ * 命名空间：$rootnamespace$
+ * 创建时间：$time$
+ * 机器名称：$machinename$
+ * 创建者：$username$
+ * 电子邮箱：fengqinhua2016@163.com
+ * 描述：
+ * 
+ **************************************************************/
+
+ 
+using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+$if$ ($targetframeworkversion$ >= 4.5)using System.Threading.Tasks;
+$endif$
+namespace $rootnamespace$
+{
+    class $safeitemrootname$
+    {
+    }
+}
+~~~
+3. 同样的方式修改Interface模板文件
+
+### 03. 配置 GitHub 和 Gitee 开发账号
+
+具体可参考：
+
+> [生成和添加 SSH 公钥](https://www.cnblogs.com/librarookie/p/15390170.html)  
+> [配置 GitHub 和 Gitee 共存环境](https://cloud.tencent.com/document/product/436/7778)  
+
+
---
+++ src/Basiclibrary/Sgr.Core/Sgr/Constant.cs
@@ -0,0 +1,48 @@
+﻿/**************************************************************
+ * 
+ * 唯一标识：576766b7-cefc-421f-bc0c-470bb0df2508
+ * 命名空间：Sgr
+ * 创建时间：2023/7/20 21:17:42
+ * 机器名称：DESKTOP-S0D075D
+ * 创建者：antho
+ * 电子邮箱：fengqinhua2016@163.com
+ * 描述：
+ * 
+ **************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sgr
+{
+    /// <summary>
+    /// 常量定义
+    /// </summary>
+    public static class Constant
+    {
+        /// <summary>
+        /// 最小时间
+        /// </summary>
+        public static readonly DateTime MinDateTime = new DateTime(1900, 1, 1, 1, 1, 1);
+        /// <summary>
+        /// 最大时间
+        /// </summary>
+        public static readonly DateTime MaxDateTime = new DateTime(2999, 1, 1, 1, 1, 1);
+        /// <summary>
+        /// 实体的扩展属性最大长度
+        /// </summary>
+        public static int ExtendableObjectMaxLength = 2047;
+
+        //public const string Setup_SiteName = "SiteName";
+        //public const string Setup_Language = "Language";
+        //public const string Setup_AdminUsername = "AdminUsername";
+        //public const string Setup_AdminUserId = "AdminUserId";
+        //public const string Setup_AdminEmail = "AdminEmail";
+        //public const string Setup_AdminPassword = "AdminPassword";
+        //public const string Setup_DatabaseProvider = "DatabaseProvider";
+        //public const string Setup_DatabaseConnectionString = "DatabaseConnectionString";
+        //public const string Setup_DatabaseTablePrefix = "DatabaseTablePrefix";
+        //public const string Setup_SiteTimeZone = "SiteTimeZone";
+    }
+}
